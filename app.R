4e808f8e5dfb169e72323c8e9d19fc034e9afcaf
Mark Shteingardt <markshteingardt@yandex.ru>
2023-02-18 12:39:03 +0000

    Updated UI, new elements and bug fixes

--- app.R
+++ app.R
@@ -10,7 +10,6 @@ source("global.R")
 ui <- dashboardPage(skin = "purple",
   dashboardHeader(title = "Menu", titleWidth = 230),
   
-  
   dashboardSidebar(
     sidebarMenu(
       menuItem("General information", tabName = "general", icon = icon("info-circle")),
@@ -37,6 +36,16 @@ ui <- dashboardPage(skin = "purple",
         }
        ")),
       
+      tags$style(HTML(".small_margin_class {margin-bottom: 5px;}")),
+      
+      tags$style(
+        HTML(".test_custom_class {
+           color: black;
+           font-size: 24px;
+           font-style: normal;
+         }")
+      ),
+      
       tags$style(HTML(".js-irs-0 .irs-single, .js-irs-0 .irs-bar-edge, 
                       .js-irs-0 .irs-bar {background: #8675A9; border: #8675A9}")),
      
@@ -46,12 +55,8 @@ ui <- dashboardPage(skin = "purple",
       # First tab content
       tabItem(tabName = "general", style = "padding: 25px",
               fluidRow(
-                
                 titlePanel("How to use this application"),
-                
-                wellPanel(
-                p(general_info_p)
-                )
+                wellPanel(p(general_info_p))
               )
       ),
       # Second tab content
@@ -70,13 +75,18 @@ ui <- dashboardPage(skin = "purple",
               
               # Optional fluid row which will render UI elements if [show network] button is pressed. 
               fluidRow(
-                mainPanel(
+                
                   conditionalPanel(
                     condition = "input.indv_net_pressed > 0",
                     
                     sidebarLayout(
                       sidebarPanel(
-                       
+                        h2(style = "color: #8675A9", "Egocentric node information:"),
+                        
+                        htmlOutput("selected_ind_info", class = "test_custom_class"),
+    
+                        uiOutput("ind_personal_link_button"),
+                  
                       ),
                       mainPanel(
                         div(style = "width: 600px; height: 600px; border: 2px solid purple;",
@@ -85,13 +95,8 @@ ui <- dashboardPage(skin = "purple",
                       )
                     )
                     
-                    
-                    
-                    
-                    
-                    
-                  )
-                )
+                  ) # End of conditional panel 
+                
               )
               
               
@@ -109,35 +114,34 @@ server <- shinyServer(function(input, output, session) {
     histogram_plot
   })
   
-  output$network_ui_elements <- renderUI({
-    if (input$choose_percentage == 0)
-      return(NULL)
+  
+  # Obserbe changes in user input and change the label accordingly
+  observeEvent(input$percent_changed, {
+  
+    updateSelectInput(session, "selectInput_changed", choices = 1:length(decomposed_g))
+    label <- paste0("Select ", input$percent_changed, "% of data")
+    updateActionButton(inputId = "choose_percentage", label = label)
+  })   
     
-    # now when the user selected the % of data, we need to load that particular data
-    decomposed_g <- load_requested_data_based_on_percent(input$percent_changed)
+  network_ui_elements <- eventReactive(input$choose_percentage, {
+    # Create UI elements here
+    decomposed_g <<- load_requested_data_based_on_percent(input$percent_changed)
     updateSelectInput(session, "selectInput_changed", choices = 1:length(decomposed_g))
     
     fluidRow(
       sidebarLayout(
         sidebarPanel(
-      
+          
           helpText(div(paste0(promt_sub_g, length(decomposed_g), " subgraphs."), 
                        style = "color: #888FF; font-size: 18px;")),
           
-          selectInput("selectInput_changed", "Select an option:", choices = c(1,2,3)),
+          selectInput("selectInput_changed", "Select an option:", choices = NULL),
           
           h2(style = "color: #8675A9", "Node information:"),
-          helpText("Note: to see the node information click on any node on the right."),
+          helpText(help_str),
           
-          htmlOutput("selected_node_info"),
-          tags$head(tags$style("#selected_node_info{color: black;
-                                 font-size: 24px;
-                                 font-style: normal;
-                                 }"
-                              )
-                    ),
-          
-          uiOutput("personal_link_button"),
+          htmlOutput("selected_node_info", class = "test_custom_class"),
+          uiOutput("personal_link_button", class = "small_margin_class"),
           uiOutput("indiv_network_button")
           
         ), # End of the sidebarPanel 
@@ -145,22 +149,17 @@ server <- shinyServer(function(input, output, session) {
         
         mainPanel(
           div(style = "width: 600px; height: 600px; border: 2px solid purple;",
-            forceNetworkOutput("subgraph_network_viz", width = "600px", height = "600px")
+              forceNetworkOutput("subgraph_network_viz", width = "600px", height = "600px")
           ),
-        
+          
         )
- 
+        
       )
     )
-    
   })
-  
-  # Obserbe changes in user input and change the label accordingly
-  
-  observeEvent(input$percent_changed, {
-    updateSelectInput(session, "selectInput_changed", choices = 1:length(decomposed_g))
-    label <- paste0("Select ", input$percent_changed, "% of data")
-    updateActionButton(inputId = "choose_percentage", label = label)
+
+  output$network_ui_elements <- renderUI({
+    network_ui_elements()
   })
   
   # Observe click on a node and present the full information on it if clicked
@@ -172,20 +171,22 @@ server <- shinyServer(function(input, output, session) {
     output$selected_node_info <- renderText({ 
       str_id <- paste("Node ID: ", selected_node)
       str_name <- paste("Name: ", node_information$node.name[1])
-      str_label <- paste("Visited: ", node_information$event.label[1])
-      str_type <- paste("Participated in: ", node_information$event.Type[1])
-      HTML(paste(str_id, str_name,str_label, str_type, sep = '<br/>'))
+      str_type <- paste("Participated in: ", nrow(node_information), " event(s).")
+      str_message <- "Visit the link below for additional information."
+      HTML(paste(str_id, str_name, str_type, str_message, sep = '<br/>'))
     })
     
     output$personal_link_button <- renderUI({
-      shiny::a(
-        h4(icon("link"),
-           paste0("Personal web-page"),
-           class = "custom_button",
-           style = "fontweight:600"),
-        target = "_blank",
-        href = personal_url
+      actionButton(
+        inputId = "personal_link_button",
+        label = "Personal web-page",
+        icon = icon("link"),
+        class = "custom_button"
       )
+    })
+    
+    observeEvent(input$personal_link_button, {
+      browseURL((personal_url))
     })
     
     # show individual network
@@ -209,14 +210,47 @@ server <- shinyServer(function(input, output, session) {
     
   })
   
+  
+  # Observe click on selected egocentric network and provide information on the node
+  observeEvent(input$ind_node_clicked, {
+    ind_n_info = selected_indv_n_df[selected_indv_n_df$node.ID == input$ind_node_clicked,]
+    egoc_ind_url <- paste(link_str, ind_n_info$node.ID[1], sep = "")
+    ind_s_node <<- ind_n_info$node.ID[1]
+  
+    output$selected_ind_info <- renderText({ 
+      str_id <- paste("Node ID: ", ind_s_node)
+      str_name <- paste("Name: ", ind_n_info$node.name[1])
+      str_type <- paste("Participated in: ", nrow(ind_n_info), " events.")
+      HTML(paste(str_id, str_name, str_type, sep = '<br/>'))
+    })
+    
+    
+    output$ind_personal_link_button <- renderUI({
+      actionButton(
+        inputId = "ind_personal_link_button",
+        label = "Personal web-page",
+        icon = icon("link"),
+        class = "custom_button"
+      )
+    })
+    
+    # open a link if it is pressed
+    observeEvent(input$ind_personal_link_button, {
+      browseURL((egoc_ind_url))
+    })
+  
+  }) # end of Observe click 
+  
+  
   # Individual node Network plotting
   output$individual_network_viz <- renderForceNetwork({
 
     sj_indiv <- create_sj_indiv(selected_node)
-
+    ind_script <- 'Shiny.onInputChange("ind_node_clicked", d.name)'
+    
     forceNetwork(Links = sj_indiv$links, Nodes = sj_indiv$nodes, Source = "source",
                  Target = "target", NodeID = "name", Group = "group",
-                 opacity = 1, zoom = T, fontSize = 0, linkDistance = 150
+                 opacity = 1, zoom = T, fontSize = 0, linkDistance = 150, clickAction = ind_script
     )
   })
   
--- global.R
+++ global.R
@@ -1,7 +1,8 @@
 # Load requested data by the user
-setwd("~/Desktop/FYP_Project/current_data_for_work")
+setwd("~/Desktop/FYP_Project/Shiny/FYP_app/application_data")
 full_data_set <- read.csv(file = 'combined_data.csv')
 promt_sub_g <- "You can choose which connected sub-graph to visualize. For the current subset of data there are "
+help_str <- "Note: to see the node information click on any node on the right."
 link_str <- "https://clericus.ie/person/"
 
 general_info_p <- paste(readLines("text.txt"), collapse = "\n")
@@ -11,7 +12,7 @@ edge_list_10p <- read.csv(file = 'edge_list_deg_81+.csv')
 edge_list_15p <- read.csv(file = 'edge_list_deg_78+.csv')
 edge_list_20p <- read.csv(file = 'edge_list_deg_73+.csv')
 edge_list_25p <- read.csv(file = 'edge_list_deg_70+.csv')
-
+decomposed_g <- decompose.graph(graph_from_data_frame(edge_list_10p, directed = FALSE, vertices = NULL))
 # data for the histogram and plot configuration 
 full_edge_list <- read.csv(file = 'NOD_EDGE_LIST_FINAL.csv')
 full_graph <- graph_from_data_frame(full_edge_list, directed = FALSE, vertices = NULL)
@@ -33,7 +34,7 @@ histogram_plot <- ggplot(nodal_d_df, aes(Var1, Freq)) +
 # End of the histogram code
 
 load_requested_data_based_on_percent <- function(selected_percent) {
-
+  
   if (selected_percent == 5) {
     edge_list <- edge_list_5p
   } else if (selected_percent == 10) {
@@ -46,16 +47,15 @@ load_requested_data_based_on_percent <- function(selected_percent) {
     edge_list <- edge_list_25p
   }
   
+  # can potentially improve two code lines below
   list_of_unique_nodes <- unique(c(edge_list$A, edge_list$B))
   selected_nodes_df <<- full_data_set[full_data_set$node.ID %in% list_of_unique_nodes,]
-  
   # Preparing a graph object for the selected file (which will be held on the server and called based on user input)
   graph_object <- graph_from_data_frame(edge_list, directed = FALSE, vertices = NULL)
-  degrees_curr_g <- degree(graph_object)
+  # degrees_curr_g <- degree(graph_object)
   decomposed_g <- decompose.graph(graph_object)
   return(decomposed_g)
 } 
-
 
 create_sj_object <- function(sub_gr_number) {
   current_subgraph <- decomposed_g[[sub_gr_number]] # Selects a subgraph chosen by the user
@@ -67,12 +67,14 @@ create_sj_object <- function(sub_gr_number) {
 create_sj_indiv <- function(selected_node_id) {
 
   egocentric_edge_l = full_edge_list[which(full_edge_list[,1] == selected_node_id),]
+  # create a data frame for individual network representation 
+  indv_unq_n <- unique(c(egocentric_edge_l$A, egocentric_edge_l$B))
+  selected_indv_n_df <<- full_data_set[full_data_set$node.ID %in% indv_unq_n,]
   
   g_o <- graph_from_data_frame(egocentric_edge_l, directed = FALSE, vertices = NULL)
   m <- membership(cluster_walktrap(g_o, steps = 1))
-  my_sj_list <- igraph_to_networkD3(g_o, group = m)
- 
-  return(my_sj_list)
+  indv_sj_list <- igraph_to_networkD3(g_o, group = m)
+  return(indv_sj_list)
 }
 
 
